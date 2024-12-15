48811bda6af73bd34ddaf71f772aec870e34ec6a
Maidan10 <aidan.marube@gmail.com>
2024-12-15 09:28:31 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+const leastFavoriteCustomer = "somebody else";
+
+function changeLeastFavoriteCustomer() {
+    // This should throw an error as reassignment of a constant is not allowed
+    leastFavoriteCustomer = "another person";
+}
